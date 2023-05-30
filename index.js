72bdefda4822d15e51bd3c1fe88d2abadab8b9f4
Marshall <marshall.peters01@gmail.com>
2023-05-30 15:39:39 -0400

    Added the viewAllEmployees function to index.js

--- index.js
+++ index.js
@@ -62,9 +62,26 @@ function viewAllDepartments() {
 
 function viewAllRoles() {
   const query = 
-  `SELECT roles.role_id, roles.title, departments.department_name, roles.salary
+    `SELECT roles.role_id, roles.title, departments.department_name, roles.salary
     FROM roles
     INNER JOIN departments ON roles.department_id = departments.department_id`;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    displayMenu();
+  });
+}
+
+function viewAllEmployees() {
+  const query = 
+    `SELECT employees.employee_id, employees.first_name, employees.last_name,
+    roles.title AS job_title, departments.department_name, roles.salary,
+    CONCAT(managers.first_name, ' ', managers.last_name) AS manager
+    FROM employees
+    INNER JOIN roles ON employees.role_id = roles.role_id
+    INNER JOIN departments ON roles.department_id = departments.department_id
+    LEFT JOIN employees AS managers ON employees.manager_id = managers.employee_id
+  `;
   connection.query(query, (err, res) => {
     if (err) throw err;
     console.table(res);
