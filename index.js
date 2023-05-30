a8c0b9fb87d1b66d5db56f5cfe9a5089ba62e596
Marshall <marshall.peters01@gmail.com>
2023-05-30 15:11:44 -0400

    Added starter code and display menu in index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const inquirer = require('inquirer');
+
+// Works with inquirer to add a selection-menu in the terminal to decide which function to execute based on user input. //
+function displayMenu() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'menuChoice',
+        message: 'Please select an option:',
+        choices: [
+          'View All Departments',
+          'View All Roles',
+          'View All Employees',
+          'Add a Department',
+          'Add a Role',
+          'Add an Employee',
+          'Update an Employee Role',
+          'Exit',
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.menuChoice) {
+        case 'View All Departments':
+          viewAllDepartments();
+          break;
+        case 'View All Roles':
+          viewAllRoles();
+          break;
+        case 'View All Employees':
+          viewAllEmployees();
+          break;
+        case 'Add a Department':
+          addDepartment();
+          break;
+        case 'Add a Role':
+          addRole();
+          break;
+        case 'Add an Employee':
+          addEmployee();
+          break;
+        case 'Update an Employee Role':
+          updateEmployeeRole();
+          break;
+        case 'Exit':
+          console.log('Exiting the application.');
+          connection.end();
+          break;
+      }
+    });
+}
+
+// Calls this function to display the main menu when the application starts. // 
+displayMenu();
